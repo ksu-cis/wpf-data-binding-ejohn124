640d5dc00f68a97b6f0fffb8ffb01aca2fcdde9e
erj2016 <erj2016@ksu.edu>
2020-04-01 21:44:13 -0500

    Finished WPF Data Binding

---
+++ UniversityRegistry/Data/EnumToBooleanConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace UniversityRegistry.Data
+{
+    public class EnumToBooleanConverter: IValueConverter
+    {
+        /// <summary>
+        /// Converts an enum value into a boolean by comparing it to the supplied parameter.  
+        /// If the value and parameter are the same, the method returns true; otherwise false
+        /// </summary>
+        /// <param name="value">The enum value to convert</param>
+        /// <param name="targetType">The type to convert to (should be a boolean)</param>
+        /// <param name="param">The enum value to compare to</param>
+        /// <param name="culture">The culture info</param>
+        /// <returns>True if the enum matches the param, false otherwise</returns>
+        public object Convert(object value, Type targetType, object param, CultureInfo culture)
+        {
+            return value.Equals(param);
+        }
+
+        /// <summary>
+        /// Converts a boolean into an enum value.  If the boolean is true, the parameter is returned,
+        /// otherwise DependencyProperty.UnsetValue is returned (which unsets the control)
+        /// </summary>
+        /// <param name="value">The boolean to convert</param>
+        /// <param name="targetType">The type to convert (should be an enum)</param>
+        /// <param name="param">The enumeration value to check</param>
+        /// <param name="culture">The culture info</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object param, CultureInfo culture)
+        {
+            if ((bool)value) return param;
+            else return DependencyProperty.UnsetValue;
+        }
+
+
+    }
+}
--- UniversityRegistry/Data/Person.cs
+++ UniversityRegistry/Data/Person.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace UniversityRegistry.Data
 {
     /// <summary>
     /// A class representing a person associated with the university
     /// </summary>
-    public class Person
+    public class Person: INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
         /// <summary>
         /// The next ID to assign to a newly-created person
         /// </summary>
@@ -19,10 +22,20 @@ namespace UniversityRegistry.Data
         /// </summary>
         public uint ID { get; private set; }
 
+        private string firstName;
         /// <summary>
         /// The person's first name
         /// </summary>
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value) return;
+                firstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
 
         /// <summary>
         /// The person's last name
@@ -39,11 +52,27 @@ namespace UniversityRegistry.Data
         /// </summary>
         public bool Active { get; set; }
 
+        private Role role;
         /// <summary>
         /// The person's role
         /// </summary>
-        public Role Role { get; set; }
-        
+        public Role Role
+        {
+            get => role;
+            set
+            {
+                if (role == value) return;
+                role = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsUndergraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsGraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFaculty"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsStaff"));
+            }
+        }
+
+
         /// <summary>
         /// Creates a new user, assigning them an ID
         /// </summary>
@@ -60,6 +89,33 @@ namespace UniversityRegistry.Data
         {
             return $"{LastName}, {FirstName} [{ID}]";
         }
+
+        public bool IsFaculty
+        {
+            get { return Role == Role.Faculty; }
+            set { Role = Role.Faculty; }
+        }
+
+        public bool IsUndergraduateStudent
+        {
+            get { return Role == Role.UndergraduateStudent; }
+            set { Role = Role.UndergraduateStudent; }
+        }
+
+        public bool IsStaff
+        {
+            get { return Role == Role.Staff; }
+            set { Role = Role.Staff; }
+        }
+
+        public bool IsGraduateStudent
+        {
+            get { return Role == Role.GraduateStudent; }
+            set { Role = Role.GraduateStudent; }
+        }
+
+
+
 
     }
 }
